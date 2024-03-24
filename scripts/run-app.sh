e9dd931d757208512de202c82d0cafddbcf4ea96
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2024-03-24 19:30:45 +0700

    make it runable

--- scripts/run-app.sh
+++ scripts/run-app.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+gstreamer_cache=$HOME/.cache/gstreamer-1.0/registry.x86_64.bin
+
+if [ -f $gstreamer_cache ]; then
+    echo "Gstreamer cache exists"
+    echo "Removing Gstreamer cache"
+    rm $gstreamer_cache
+fi
+
 config="config/dstest2_config.yml"
 
 if [ "$1" == "Release" ]; then

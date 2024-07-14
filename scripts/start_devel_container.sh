a3ec425e1cb360c3267022b50fbf2802be1c8512
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2024-07-14 10:50:55 +0700

    update start container script to include more project

--- scripts/start_devel_container.sh
+++ scripts/start_devel_container.sh
@@ -4,9 +4,9 @@ xhost +
 
 docker run -itd --gpus all --ipc=host \
 	-e DISPLAY=$DISPLAY \
+	-e TZ=Asia/Bangkok \
 	--name ds-practice-container \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v /etc/localtime:/etc/localtime:ro \
-	-v /etc/timezone:/etc/timezone:ro \
-	-v $(pwd):/home/triton-server/ds-practice \
+	-v /home/witoon/programming-practice/ds-practice:/home/triton-server/ds-practice \
+	-v /home/witoon/programming-practice/parking-lot-monitor:/home/triton-server/parking-lot-monitor \
 	ds-workspace:latest /bin/bash
